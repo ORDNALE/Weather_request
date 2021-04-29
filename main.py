11d5e257a6c02a325b613006a53f5cc36e666ad9
Elandro <elandro10@outlook.com>
2021-04-29 12:23:11 -0400

    inserção interface grafica

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 # pip install bs4
-# pip install resquests
+# pip install requests
 from bs4 import BeautifulSoup
 import requests
+import PySimpleGUI as sg
 
-search = 'clima cuiaba'
+# inseringo interface grafica com PysimpleGUI
 
-url = f'https://www.google.com/search?q={search}'
+campos = [
+    [sg.Text('Escreva o nome da cidade')],
+    [sg.Text('Ex: "Rio de janeio, Paris, Moscow"')],
+    [sg.Input(key='local')],
+    [sg.Output(key='resultado')],
+    [sg.Button('Buscar'), sg.Button('Fechar'), sg.Button('Limpar')]
+]
+# Criando Janela
+janela = sg.Window('Informação do Clima', campos)
 
-r = requests.get(url)
-s = BeautifulSoup(r.text, 'html.parser')
-info = s.find('div', class_='BNeawe').text
 
-print(info)
+def temperatura(valor):
+    # Faz uma busca no google e obtem a temperatura da cidade informada
+    pesquisa = valores['local']
+    url = f'https://www.google.com/search?q=clima+{pesquisa}'
+    r = requests.get(url)
+    s = BeautifulSoup(r.text, 'html.parser')
+    info = s.find('div', class_='BNeawe').text
+    return info
+
+
+while True:
+    # faz a leitura do layout
+    eventos, valores = janela.read()
+    try:
+        # Encerra aplicação
+        if eventos == sg.WINDOW_CLOSED or eventos == 'Fechar':
+            break
+        # Mostra na tela Resultado
+        elif eventos == 'Buscar' and valores['local'] != "":
+            print(temperatura(valores))
+        # Limpa os Campos
+        elif eventos == 'Limpar':
+            janela['local'].update('')
+            janela['resultado'].update('')
+            valores['local'] = ""
+
+    except:
+        print('Operação invalida')
+        print('Erro: Verifique se digitou a cidade corretamente')
